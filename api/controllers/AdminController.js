e1e49ce7225da967a08b9999e2e1ccab3724f2b3
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-01-31 17:30:39 +0400

    Create update/create/delete sliders

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -27,5 +27,38 @@ module.exports = {
         } else {
             res.json(400, Msg.getMessage(400))
         }
+    },
+    createSlider: function(req, res) {
+        Sliders.create(req.body).exec(function(err, data) {
+            if (err) return res.serverError(err)
+
+            res.json(data)
+        })
+    },
+    updateSlider: function(req, res) {
+        if (req.body.id) {
+            Sliders.update({
+                id: req.body.id
+            }, req.body).exec(function(err, data) {
+                if (err) return res.serverError(err)
+
+                res.json(data)
+            })
+        } else {
+            res.json(400, Msg.getMessage(400))
+        }
+    },
+    deleteSlider: function(req, res) {
+        if (req.body.id) {
+            Sliders.destroy({
+                id: req.body.id
+            }).exec(function(err, data) {
+                if (err) return res.serverError(err)
+
+                res.json(data)
+            })
+        } else {
+            res.json(400, Msg.getMessage(400))
+        }
     }
 }
