41a8e07218f7a81a5f002d7815259dea905ad85b
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-01-31 12:10:18 +0400

    Remove files

--- api/migrations/03-Shedule-Repeat.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `schedule` ADD `repeat` INT NOT NULL DEFAULT '0' AFTER `state`;
--- api/policies/manageAuth.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next) {
-    if (req.user.role == SURPER_ADMIN_ROLE_ID) {
-        next()
-    } else {
-        res.forbidden('Not permission on this action');
-    }
-};
