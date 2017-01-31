45e44c33f225d61be671cc8e5a937a34d678f39f
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-01-31 18:11:08 +0400

    Upload image action

--- api/controllers/PublicController.js
+++ api/controllers/PublicController.js
@@ -18,5 +18,18 @@ module.exports = {
 
             res.json(data)
         })
+    },
+    uploadImages: function(req, res) {
+        req.file('file').upload({
+            dirname: '../../protected_files/'
+        }, function(error, uploadedFiles) {
+            if (error) return res.serverError(error);
+
+            var imageName = uploadedFiles[0].fd.split("protected_files/")[1];
+
+            var url = DOMAIN_NAME + "/get/" + imageName;
+
+            res.json(url);
+        });
     }
 }
