6aa43b7c1e4ba4376012cf20ed88ea7b9d5181ce
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-02-03 16:14:12 +0400

    Remove creeate about as

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -1,18 +1,4 @@
 module.exports = {
-    createAboutUs: function(req, res) {
-        if (req.body.text) {
-
-            AboutUs.create({
-                text: req.body.text
-            }).exec(function(err, data) {
-                if (err) return res.serverError(err)
-
-                res.json(data)
-            })
-        } else {
-            res.json(400, Msg.getMessage(400))
-        }
-    },
     updateAboutUs: function(req, res) {
         if (req.body.id && req.body.text) {
             AboutUs.update({
